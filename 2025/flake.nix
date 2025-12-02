cd12176c812c53ed89745913e7d52f4ec573b080
Thomas Cremel <thomas.cremel@epita.fr>
2025-12-02 12:03:08 +0100

    2025: flake: add treefmt

--- 2025/flake.nix
+++ 2025/flake.nix
@@ -1,27 +1,49 @@
 {
   description = "AoC 2025 with OCaml";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/x86_64-linux";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
     {
       self,
       nixpkgs,
+      systems,
+      treefmt-nix,
       ...
     }:
     let
-      forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+      lib = nixpkgs.lib;
+
+      eachSystem = f: lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      treefmt = eachSystem (
+        pkgs:
+        treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            ocamlformat.enable = true;
+          };
+        }
+      );
     in
     {
       # nix run . -- <day> [<filename>]
-      packages = forAllSystems (
-        system: pkgs:
+      packages = eachSystem (
+        pkgs:
         let
           lib = pkgs.lib;
           fs = lib.fileset;
         in
         {
-          default = self.packages.${system}.aoc;
+          default = self.packages.${pkgs.system}.aoc;
           aoc = pkgs.ocamlPackages.buildDunePackage {
             pname = "aoc";
             version = "0.1.0";
@@ -53,27 +75,29 @@
         }
       );
 
-      devShells = forAllSystems (
-        system: pkgs: {
-          default = pkgs.mkShellNoCC {
-            packages = builtins.attrValues {
-              inherit (pkgs)
-                dune_3
-                # opam
-                ocaml
-                ;
-              inherit (pkgs.ocamlPackages)
-                cohttp-lwt-unix
-                lwt_ssl
-                ;
-              inherit (pkgs.ocamlPackages.janeStreet)
-                ppx_expect
-                ;
-            };
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = builtins.attrValues {
+            inherit (pkgs)
+              dune_3
+              # opam
+              ocaml
+              ;
+            # inherit (pkgs.ocamlPackages)
+            #   cohttp-lwt-unix
+            #   lwt_ssl
+            #   ;
+            # inherit (pkgs.ocamlPackages.janeStreet)
+            #   ppx_expect
+            #   ;
           };
-        }
-      );
+        };
+      });
 
-      formatter = forAllSystems (_: pkgs: pkgs.nixfmt-tree);
+      formatter = eachSystem (pkgs: treefmt.${pkgs.system}.config.build.wrapper);
+
+      checks = eachSystem (pkgs: {
+        formatting = treefmt.${pkgs.system}.config.build.check self;
+      });
     };
 }
