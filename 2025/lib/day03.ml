f8bb01ba5178b0f8dd312f20a8a67c14344e219c
Ratakor <ratakor@disroot.org>
2025-12-03 20:58:40 +0100

    2025: day 03 part 2

--- 2025/lib/day03.ml
+++ 2025/lib/day03.ml
@@ -5,29 +5,40 @@ module Day03 : Day.Day = struct
     |> List.of_seq
     |> List.map (fun c -> int_of_char c - int_of_char '0')
 
-  let find_jolt digits =
-    let rec aux max i j =
-      if j = List.length digits then
-        if i + 2 = List.length digits then max
-        else
-          let n = (10 * List.nth digits (i + 1)) + List.nth digits (i + 2) in
-          if n > max then aux n (i + 1) (i + 3) else aux max (i + 1) (i + 3)
-      else
-        let n = (10 * List.nth digits i) + List.nth digits j in
-        if n > max then aux n i (j + 1) else aux max i (j + 1)
-    in
-    aux 0 0 1
+  let count_digits n =
+    (n |> float_of_int |> log10) +. 1. |> floor |> int_of_float
 
-  let solve filename =
+  (* i to j included *)
+  let subseq i j seq = seq |> Seq.drop i |> Seq.take (j - i + 1)
+
+  let find_jolt digits n =
+    let rec aux acc first_idx =
+      let digit_count = count_digits acc in
+      if digit_count = n then acc
+      else
+        let last_idx = List.length digits - (n - digit_count) in
+        let max_idx, max_digit =
+          digits
+          |> List.to_seq
+          |> subseq first_idx last_idx
+          |> Seq.fold_lefti
+               (fun max idx digit ->
+                 if digit > snd max then (idx, digit) else max)
+               (-1, -1)
+        in
+        aux ((10 * acc) + max_digit) (first_idx + max_idx + 1)
+    in
+    aux 0 0
+
+  let solve filename n =
     filename
     |> Utils.Input.read_file_to_string
     |> Utils.Input.tokenize_on_char '\n'
     |> List.map digits_of_string
-    |> List.map find_jolt
-    |> List.fold_left ( + ) 0
+    |> List.fold_left (fun acc digits -> acc + find_jolt digits n) 0
 
-  let part1 filename = solve filename |> Printf.printf "Part 1: %d\n"
-  let part2 filename = solve filename |> Printf.printf "Part 2: %d\n"
+  let part1 filename = solve filename 2 |> Printf.printf "Part 1: %d\n"
+  let part2 filename = solve filename 12 |> Printf.printf "Part 2: %d\n"
 end
 
 let () = Days.register "3" (module Day03)
