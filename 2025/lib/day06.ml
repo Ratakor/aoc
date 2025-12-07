91e099014c775eebdb6f29e9cab58065f33c1a9d
Ratakor <ratakor@disroot.org>
2025-12-07 01:11:45 +0100

    2025: day 06 part 2

--- 2025/lib/day06.ml
+++ 2025/lib/day06.ml
@@ -1,41 +1,65 @@
 module Impl = struct
+  let rtrim s = Str.replace_first (Str.regexp {| +$|}) "" s
+  let rpad c n s = s ^ String.init (n - String.length s) (fun _ -> c)
+
+  (* this mf doesn't know about List.filter (fun c -> c <> ' ') *)
+  let rec to_chunks acc seq =
+    match seq () with
+    | Seq.Nil -> acc
+    | Seq.Cons ((i, '+'), next) -> to_chunks (('+', (i, 0)) :: acc) next
+    | Seq.Cons ((i, '*'), next) -> to_chunks (('*', (i, 0)) :: acc) next
+    | Seq.Cons ((_, ' '), next) -> (
+        match acc with
+        | [] -> failwith "Input doesn't start with an op"
+        | (op, (i, len)) :: tl -> to_chunks ((op, (i, len + 1)) :: tl) next)
+    | _ -> failwith "Invalid char"
+
   let parse input =
     let lines =
       input
-      |> Utils.Input.tokenize_on_char '\n'
-      |> List.rev
-      |> List.map (Utils.Input.tokenize_on_char ' ')
+      |> String.split_on_char '\n'
+      |> List.map rtrim
+      |> List.filter (fun x -> String.length x > 0)
     in
-    let numbers = lines |> List.tl |> List.map (List.map int_of_string) in
-    let operations =
+    let max_len =
       lines
-      |> List.hd
-      |> List.map (fun s ->
-          match String.get s 0 with
-          | '+' -> ( + )
-          | '*' -> ( * )
-          | _ -> failwith "Unexpected op")
+      |> List.fold_left (fun len s -> max len (String.length s)) 0
+      |> ( + ) 1 (* padding *)
     in
-    (numbers, operations)
+    lines |> List.map (fun s -> rpad ' ' max_len s) |> List.rev |> function
+    | hd :: tl -> (hd |> String.to_seqi |> to_chunks [], List.rev tl)
+    | _ -> failwith "Invalid input"
 
-  let[@tail_mod_cons] rec mapf2 lf l1 l2 =
-    match (lf, l1, l2) with
-    | [], [], [] -> []
-    | f :: lf, a :: l1, b :: l2 ->
-        let r = f a b in
-        r :: mapf2 lf l1 l2
-    | _, _, _ -> invalid_arg "mapf2"
+  let fold_op op l =
+    match op with
+    | '+' -> List.fold_left ( + ) 0 l
+    | '*' -> List.fold_left ( * ) 1 l
+    | _ -> failwith "Invalid op"
 
   let part1 input =
-    let numbers, operations = parse input in
-    numbers
-    |> List.tl
-    |> List.fold_left (fun acc n -> mapf2 operations acc n) (List.hd numbers)
+    let chunks, numbers = parse input in
+    chunks
+    |> List.map (fun (op, (pos, len)) ->
+        numbers
+        |> List.map (fun row ->
+            String.sub row pos len |> String.trim |> int_of_string)
+        |> fold_op op)
     |> List.fold_left ( + ) 0
 
   let part2 input =
-    ignore input;
-    0
+    let chunks, numbers = parse input in
+    chunks
+    |> List.map (fun (op, (pos, len)) ->
+        List.init len (fun i -> pos + i)
+        |> List.map (fun col ->
+            numbers
+            |> List.map (fun row -> String.get row col)
+            |> List.to_seq
+            |> String.of_seq
+            |> String.trim
+            |> int_of_string)
+        |> fold_op op)
+    |> List.fold_left ( + ) 0
 end
 
 module Day06 : Day.Solution = Impl
