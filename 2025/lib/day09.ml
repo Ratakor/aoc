f518468b8d2dd054ae83796f9adebf6e27d6635c
Ratakor <ratakor@disroot.org>
2025-12-09 14:43:36 +0100

    2025: improve day 09

--- 2025/lib/day09.ml
+++ 2025/lib/day09.ml
@@ -2,19 +2,27 @@ module Impl = struct
   let parse input =
     input
     |> Utils.Input.tokenize_on_char '\n'
-    |> List.map (fun line -> Scanf.sscanf line "%d,%d" (fun x y -> (x, y)))
+    |> List.map (fun line ->
+        line
+        |> String.split_on_char ','
+        |> List.map int_of_string
+        |> function
+        | [ x; y ] -> (x, y)
+        | _ -> failwith "Invalid point")
+
+  let area (x1, y1) (x2, y2) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
 
   let part1 input =
     let points = parse input in
     points
     |> List.fold_left
-         (fun area (x1, y1) ->
+         (fun (a, i) p ->
            points
-           |> List.fold_left
-                (fun area (x2, y2) ->
-                  max area ((abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)))
-                area)
-         0
+           |> List.drop i
+           |> List.fold_left (fun a q -> max a (area p q)) a
+           |> fun a -> (a, i + 1))
+         (0, 1)
+    |> fst
 
   let part2 input =
     let points = parse input in
