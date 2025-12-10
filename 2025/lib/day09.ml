ad18391a34587abb3be7ea0f10cd5b80b9c48c54
Ratakor <ratakor@disroot.org>
2025-12-10 02:14:09 +0100

    2025: day 09 part 2

--- 2025/lib/day09.ml
+++ 2025/lib/day09.ml
@@ -11,23 +11,44 @@ module Impl = struct
         | _ -> failwith "Invalid point")
 
   let area (x1, y1) (x2, y2) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
+  let min_max a b = if a < b then (a, b) else (b, a)
+
+  let rec combinations = function
+    | [] -> []
+    | a :: tl -> List.map (fun b -> (a, b)) tl @ combinations tl
+
+  let pairwise l =
+    let hd = List.hd l in
+    let rec aux = function
+      | [] -> []
+      | [ lt ] -> [ (lt, hd) ]
+      | a :: b :: tl -> (a, b) :: aux (b :: tl)
+    in
+    aux l
 
   let part1 input =
-    let points = parse input in
-    points
-    |> List.fold_left
-         (fun (a, i) p ->
-           points
-           |> List.drop i
-           |> List.fold_left (fun a q -> max a (area p q)) a
-           |> fun a -> (a, i + 1))
-         (0, 1)
-    |> fst
+    input
+    |> parse
+    |> combinations
+    |> List.fold_left (fun a (p, q) -> max a (area p q)) 0
 
   let part2 input =
     let points = parse input in
-    ignore points;
-    0
+    let connections =
+      points
+      |> pairwise
+      |> List.map (fun ((x1, y1), (x2, y2)) ->
+          let xmin, xmax = min_max x1 x2 and ymin, ymax = min_max y1 y2 in
+          ((xmin, ymin), (xmax, ymax)))
+    in
+    points
+    |> combinations
+    |> List.filter (fun ((x1, y1), (x2, y2)) ->
+        let xmin, xmax = min_max x1 x2 and ymin, ymax = min_max y1 y2 in
+        connections
+        |> List.for_all (fun ((cxmin, cymin), (cxmax, cymax)) ->
+            not (xmin < cxmax && xmax > cxmin && ymin < cymax && ymax > cymin)))
+    |> List.fold_left (fun a (p, q) -> max a (area p q)) 0
 end
 
 module Day09 : Day.Solution = Impl
